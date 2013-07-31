45bcbd16b085d30dc172a17c212f077bf21e20a6
U-Joel-PC\Joel <Joel@Joel-PC.(none)>
2013-07-31 22:55:33 +0200

    Added mysql-connector

--- glassfish.sh
+++ glassfish.sh
@@ -10,5 +10,5 @@ export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 ~/glassfish/glassfish3/bin/asadmin start-domain
 ~/glassfish/glassfish3/glassfish/bin/asadmin set "server.java-config.java-home=/usr/lib/jvm/java-7-openjdk-amd64"
 ~/glassfish/glassfish3/bin/asadmin stop-domain
-cp /vagrant/mysql-connector-java-5.0.8/mysql-connector-java-5.0.8-bin.jar ~/glassfish/glassfish3/glassfish/domains/domain1/lib/ext/
+cp /vagrant/mysql-connector-java-5.0.8-bin.jar ~/glassfish/glassfish3/glassfish/domains/domain1/lib/ext/
 ~/glassfish/glassfish3/bin/asadmin start-domain

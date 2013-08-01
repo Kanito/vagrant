543897231be39eee3ca5dcb496348627ee0378b5
vagrant <vagrant@precise64.(none)>
2013-08-01 08:48:36 +0000

    Git configuration updates

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 echo "Configuring Git..."
-sudo su - jenkins
-git config --global user.email "jenkinsuser@example.com"
-git config --global user.name "Jenkins User"
+sudo su jenkins -c "git config --global user.email \"jenkinsuser@example.com\""
+sudo su jenkins -c "git config --global user.name \"Jenkins User\""

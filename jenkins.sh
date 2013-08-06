6f115a364f1ae00f64d1e077196b40a028e19b75
U-ERICSSON\eanjoel <eanjoel@E7E4115B4C4FB2.ericsson.se>
2013-08-06 11:12:04 +0200

    Changed to install version 1.525 of Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 echo "Installing Jenkins..."
+mkdir /home/vagrant/jenkinsinstall
+cd /home/vagrant/jenkinsinstall
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install -y jenkins
+sudo apt-get install daemon
+wget http://pkg.jenkins-ci.org/debian/binary/jenkins_1.525_all.deb
+sudo dpkg -i jenkins_1.525_all.deb
+echo "Jenkins has been successfully installed! :-)"

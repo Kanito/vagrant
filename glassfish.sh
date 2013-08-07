9c96b4ee612a0243b9c4de3cacb882e26eb6acea
Joel <joel.v.andersson@gmail.com>
2013-08-07 12:56:24 +0200

    Added glassfish as service

--- glassfish.sh
+++ glassfish.sh
@@ -9,6 +9,11 @@ cp /vagrant/domain.xml /home/vagrant/glassfish/glassfish3/glassfish/domains/doma
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 /home/vagrant/glassfish/glassfish3/bin/asadmin start-domain
 /home/vagrant/glassfish/glassfish3/glassfish/bin/asadmin set "server.java-config.java-home=/usr/lib/jvm/java-7-openjdk-amd64"
-/home/vagrant/glassfish/glassfish3/bin/asadmin stop-domain
+/home/vagrant/glassfish/glassfish3/glassfish/bin/asadmin create-service domain1
+/etc/init.d/GlassFish_domain1 stop
 cp /vagrant/mysql-connector-java-5.0.8-bin.jar /home/vagrant/glassfish/glassfish3/glassfish/domains/domain1/lib/ext/
-/home/vagrant/glassfish/glassfish3/bin/asadmin start-domain
+/etc/init.d/GlassFish_domain1 start
+
+#/home/vagrant/glassfish/glassfish3/bin/asadmin stop-domain
+#cp /vagrant/mysql-connector-java-5.0.8-bin.jar /home/vagrant/glassfish/glassfish3/glassfish/domains/domain1/lib/ext/
+#/home/vagrant/glassfish/glassfish3/bin/asadmin start-domain

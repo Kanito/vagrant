276c7d6e8ec1eb0d7adafba99659a9342242e139
Joel <joel.v.andersson@gmail.com>
2013-08-09 10:27:55 +0200

    Now downloading jenkins plugins from cyberspace

--- jenkins2.sh
+++ jenkins2.sh
@@ -28,9 +28,22 @@ cd /home/vagrant/jenkinscli
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 
 # install all plugins
-sudo su jenkins -c "cp /vagrant/jenkinsplugins/*.hpi /var/lib/jenkins/plugins/"
+mkdir /home/vagrant/jenkinsplugins
+cd /home/vagrant/jenkinsplugins
+wget http://updates.jenkins-ci.org/download/plugins/build-pipeline-plugin/1.3.5/build-pipeline-plugin.hpi
+wget http://updates.jenkins-ci.org/download/plugins/clone-workspace-scm/0.5/clone-workspace-scm.hpi
+wget http://updates.jenkins-ci.org/download/plugins/credentials/1.6/credentials.hpi
+wget http://updates.jenkins-ci.org/download/plugins/deploy/1.9/deploy.hpi
+wget http://updates.jenkins-ci.org/download/plugins/git/1.4.0/git.hpi
+wget http://updates.jenkins-ci.org/download/plugins/git-client/1.1.2/git-client.hpi
+wget http://updates.jenkins-ci.org/download/plugins/greenballs/1.12/greenballs.hpi
+wget http://updates.jenkins-ci.org/download/plugins/jquery/1.7.2-1/jquery.hpi
+wget http://updates.jenkins-ci.org/download/plugins/parameterized-trigger/2.18/parameterized-trigger.hpi
+wget http://updates.jenkins-ci.org/download/plugins/selenium/2.2/selenium.hpi
+sudo su jenkins -c "cp /home/vagrant/jenkinsplugins/*.hpi /var/lib/jenkins/plugins/"
 
 # setup jenkins configuration
+cd /home/vagrant/jenkinscli
 sudo su jenkins -c "cp /vagrant/jenkinsjobs/config.xml /var/lib/jenkins/"
 sudo su jenkins -c "cp /vagrant/jenkinsjobs/hudson.tasks.Maven.xml /var/lib/jenkins/"
 
